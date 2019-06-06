b1f155ed88d90a5a383d3b39a3be7c7c287a2fe9
admin <admin@PC-A196.local>
2019-06-06 21:22:41 +0900

    edited scrape_onsen_html.py

--- onsen_inns/inn_data_collection/scrape_onsen_html.py
+++ onsen_inns/inn_data_collection/scrape_onsen_html.py
@@ -9,8 +9,9 @@ output : multiple inns' data, like, (innA(a, b, c..), innB(a, b, c..), innC(a, b
 '''
 
 import os
+import re
 import sys, getopt
-import urllib.request
+import urllib.request as req
 from bs4 import BeautifulSoup
 from scrape_inn_html import scrapeInnHtml
 
@@ -21,12 +22,35 @@ s')
 import django
 django.setup()
 
+
+
 def scrapeOnsenHtml(url):
     inn_data = []
+    url = "https://www.jalan.net/onsen/OSN_50350.html"
+    res = req.urlopen(url)
+    soup = BeautifulSoup(res, "html.parser")
+    for link in zip(soup.find_all("td",{"class":"hotel-name"}), soup.find_all("td",{"class":"charge"})):
+        yad_num = link[0].find("a")
+        yad_num.get("href")
+        #print(yad_num.get("href"))
+        yad_num.get_text()
+        #print(yad_num.get_text())
+        yad_price = link[1].get_text()
+        yad_price = yad_price[3:-13]
+        #print("https://www.jaran.net"+yad_num.get("href"))
+        inn_one = [removeBadChars(yad_num.get_text()),yad_num.get("href"), yad_price]
+        inn_one = inn_one + scrapeInnHtml("https://www.jaran.net"+yad_num.get("href"))
+        inn_data.append(inn_one)
     return inn_data
 
+def removeBadChars(str):
+   bad_chars = ["&nbsp", "\u3000", "\n"]
+   for i in bad_chars:
+       str = str.replace(i, "")
+   return str
+
 if __name__ == "__main__":
-    
+
     url = "https://www.jalan.net/onsen/OSN_50350.html"
     inn_data = scrapeOnsenHtml(url)
     print(inn_data)
