f54e885131554546ec90c0dd6512e34bc7a33351
ktakuyabre <ktakuya.forest@gmail.com>
2019-06-18 18:27:12 +0900

    Update OnsenHome.vue

--- frontend/src/components/OnsenHome.vue
+++ frontend/src/components/OnsenHome.vue
@@ -4,44 +4,9 @@
     <div>
       <v-btn small href='/#/tinder/'>Tinder</v-btn>
     </div>
-    <template>
-  <v-layout>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-container grid-list-sm fluid>
-          <v-layout row wrap>
-            <v-flex
-              v-for="n in 9"
-              :key="n"
-              xs4
-              d-flex
-            >
-              <v-card flat tile class="d-flex">
-                <v-img
-                  :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                  :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-                  aspect-ratio="1"
-                  class="grey lighten-2"
-                >
-                  <template v-slot:placeholder>
-                    <v-layout
-                      fill-height
-                      align-center
-                      justify-center
-                      ma-0
-                    >
-                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                    </v-layout>
-                  </template>
-                </v-img>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card>
-    </v-flex>
-  </v-layout>
-</template>
+    styleItem = {
+     'backgroundImage' : 'url(' + require("https://www.pakutaso.com/shared/img/thumb/HOTE86_rotenburotobijyo15103631_TP_V.jpg") + ')'
+}
   </div>
 </template>
 
