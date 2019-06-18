9aa1ff8e2fb9c47f99be65d136d5ca9d90fccb9e
ktakuyabre <ktakuya.forest@gmail.com>
2019-06-18 19:20:25 +0900

    Update OnsenHome.vue

--- frontend/src/components/OnsenHome.vue
+++ frontend/src/components/OnsenHome.vue
@@ -7,28 +7,6 @@
   </div>
 </template>
 
-<style>
-  .bg{
-    background-image: url("./images/image.png")
-  }
-</style>
-<div class="bg" />
-
-<div style="background-image: url('https://raw.githubusercontent.com/ktakuyabre/onsen_inn_search/master/frontend/static/UNADJUSTEDNONRAW_thumb_1.jpg');" />
-
-<script>
-import AssetsImage from "https://raw.githubusercontent.com/ktakuyabre/onsen_inn_search/master/frontend/static/UNADJUSTEDNONRAW_thumb_1.jpg";
-export default {
-  data() {
-    return {
-      assetsImage: AssetsImage,
-      assetsImage_NG: "https://raw.githubusercontent.com/ktakuyabre/onsen_inn_search/master/frontend/static/UNADJUSTEDNONRAW_thumb_1.jpg",
-      staticImage: "https://raw.githubusercontent.com/ktakuyabre/onsen_inn_search/master/frontend/static/UNADJUSTEDNONRAW_thumb_1.jpg"
-    };
-  }
-};
-</script>
-
 <script>
 export default {
   name: 'OnsenHome',
