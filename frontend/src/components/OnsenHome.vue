dc27d3e04ad12a9daf021f5d26f418f33f719f01
ktakuyabre <ktakuya.forest@gmail.com>
2019-06-20 16:54:57 +0900

    Update OnsenHome.vue

--- frontend/src/components/OnsenHome.vue
+++ frontend/src/components/OnsenHome.vue
@@ -4,12 +4,10 @@
     <div>
       <v-btn small href='/#/tinder/'>Tinder</v-btn>
       <div class="bg" />
+      <ul>
+        <li><img src="/Users/admin/Documents/GitHub/onsen_inn_search/frontend/src/components/UNADJUSTEDNONRAW_thumb_1.jpg"/></li>
+      </ul>
     </div>
-  </div>
-  <div>
-    <ul>
-      <li><img src="/Users/admin/Documents/GitHub/onsen_inn_search/frontend/src/components/UNADJUSTEDNONRAW_thumb_1.jpg"/></li>
-    </ul>
   </div>
 </template>
 
