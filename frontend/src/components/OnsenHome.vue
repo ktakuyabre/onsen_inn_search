96202ec5fb38df223a05f509ad5f00e0583b067c
ktakuyabre <ktakuya.forest@gmail.com>
2019-06-20 17:49:35 +0900

    Update OnsenHome.vue

--- frontend/src/components/OnsenHome.vue
+++ frontend/src/components/OnsenHome.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
+  <body background="https://www.pakutaso.com/shared/img/thumb/HOTE86_rotenburotobijyo15103631_TP_V.jpg">
+  背景に指定した画像が繰り返し並びます。
+  </body>
     <h1>{{ msg }}</h1>
     <div>
       <v-btn small href='/#/tinder/'>Tinder</v-btn>
       <div class="bg" />
-      <body style="background: https://www.pakutaso.com/shared/img/thumb/HOTE86_rotenburotobijyo15103631_TP_V.jpg;"></li>
     </div>
   </div>
 </template>
