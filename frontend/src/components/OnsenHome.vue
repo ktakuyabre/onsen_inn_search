463defe036e3a0ea2e2190e02f2eb29ac0c70822
ktakuyabre <ktakuya.forest@gmail.com>
2019-06-20 17:12:30 +0900

    Update OnsenHome.vue

--- frontend/src/components/OnsenHome.vue
+++ frontend/src/components/OnsenHome.vue
@@ -5,7 +5,7 @@
       <v-btn small href='/#/tinder/'>Tinder</v-btn>
       <div class="bg" />
       <ul>
-        <li><img src="/Users/admin/Documents/GitHub/onsen_inn_search/frontend/src/assets/onsen_woman.png"/></li>
+        <li>h('img', { attrs: { src: require('./image.png') }})</li>
       </ul>
     </div>
   </div>
