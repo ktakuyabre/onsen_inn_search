45e51888b5a8cea7c2471b7aa8445ea8f8716edc
芦田聖太 <>
2019-07-11 13:05:18 +0900

    for check master

--- frontend/src/components/Tinder.vue
+++ frontend/src/components/Tinder.vue
@@ -1,23 +1,57 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
+    <div style="position: absolute;" v-drag>
+      <img :src="tests[count]"/>
+    </div>
+    <v-btn @click=increment color="error">Nope</v-btn>
+    <v-btn @click=increment color="info">Like</v-btn>
   </div>
 </template>
 
 <script>
+import drag from '@branu-jp/v-drag'
+
 export default {
   name: 'Tinder',
+  directives: {
+    drag,
+  },
   data () {
     return {
-      msg: 'Tinder Page',
+      msg: 'Onsender Page',
+      count: 0,
+      test: require('../assets/onsen_woman.png'),
+      pathes: {
+        p1: require('../assets/onsen_woman.png'),
+        p2: require('../assets/onei.jpg'),
+        p3: require('../assets/logo.png'),
+      },
+      tests: [
+        require('../assets/onsen_woman.png'),
+        require('../assets/onei.jpg'),
+        require('../assets/logo.png'),
+      ],
     }
   },
+  methods: {
+    // ボタンをクリックしたときのハンドラ
+    increment: function () {
+      this.count += 1 // 処理は再代入するだけでOK！
+    },
+  },
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1 {
   font-weight: normal;
+}
+li {
+  cursor:pointer;
+  padding: 10px;
+  border: solid #ddd 1px;
 }
 </style>
